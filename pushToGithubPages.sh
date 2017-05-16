f76fcf5d74b88922c02746cd9b5aea83fc2c2382
at055612 <at055612@gmail.com>
2017-05-16 10:57:50 +0100

    Comment out lines in pushToGithubPages

--- pushToGithubPages.sh
+++ pushToGithubPages.sh
@@ -5,17 +5,20 @@
 
 
 #Create a new empty gh-pages branch
-git checkout --orphan gh-pages
-git reset
-git commit --allow-empty -m 'Initial commit'
-git checkout --force master
+#git checkout --orphan gh-pages
+#git reset
+#git commit --allow-empty -m 'Initial commit'
+#git checkout --force master
 
 #ensure 'build' is in .gitignore
 
 #create directory build that is backed by the gh-pages branch
-git worktree add build gh-pages
+#git worktree add build gh-pages
+
+#Install gitbook modules if it has never been done before
+#gitbook install && gitbook build
 
 #build the gitbook and copy it into the build dir
-gitbook install && gitbook build
+gitbook build
 gitbook pdf ./ ./stroom-docs.pdf
 rsync -avr --delete --exclude '.git' ./_book/ ./build/
