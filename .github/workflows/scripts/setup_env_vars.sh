736aa352cdca2f5f9220407ffc6e4e1cf2168dd1
at055612 <22818309+at055612@users.noreply.github.com>
2021-06-18 09:17:11 +0100

    Refactor workflow

---
+++ .github/workflows/scripts/setup_env_vars.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# Set variables in github's special env file which are then automatically 
+# read into env vars in each subsequent step
+
+{
+  # Map the GITHUB env vars to our own
+  echo "BUILD_DIR=${GITHUB_WORKSPACE}"
+  echo "BUILD_COMMIT=${GITHUB_SHA}"
+
+  # Travis got to a build number of 354 so add that
+  build_number="$((GITHUB_RUN_NUMBER + 354))"
+  echo "BUILD_NUMBER=${build_number}"
+
+
+  echo "ACTIONS_SCRIPTS_DIR=${GITHUB_WORKSPACE}/.github/workflows/scripts"
+
+  # If we switch to explicitly tagging releases then we will need this
+  # if [[ ${GITHUB_REF} =~ ^refs/tags/ ]]; then
+  #   # strip off the 'refs/tags/' bit
+  #   tag="${GITHUB_REF#refs/tags/}"
+  #   echo "BUILD_TAG=${tag}"
+  # fi
+
+  if [[ ${GITHUB_REF} =~ ^refs/heads/ ]]; then
+    # strip off the 'ref/heads/' bit
+    echo "BUILD_BRANCH=${GITHUB_REF#refs/heads/}"
+  fi
+
+  # Every commit to master is a new release, at least at the moment
+  if [[ ${BUILD_BRANCH} = "master" ]]; then
+    echo "BUILD_IS_RELEASE=true"
+    echo "BUILD_TAG=stroom-docs-v${build_number}"
+  else
+    echo "BUILD_IS_RELEASE=false"
+  fi
+
+  if [[ ${GITHUB_REF} =~ ^refs/pulls/ ]]; then
+    echo "BUILD_IS_PULL_REQUEST=true"
+  else
+    echo "BUILD_IS_PULL_REQUEST=false"
+  fi
+} >> "${GITHUB_ENV}"
