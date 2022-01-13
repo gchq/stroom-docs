0e624654394bfb6cefff07778c4184a364083f9c
at055612 <22818309+at055612@users.noreply.github.com>
2022-01-13 17:41:58 +0000

    Change CI build to build all versions

--- ci_build.sh
+++ ci_build.sh
@@ -44,59 +44,154 @@ setup_ssh_agent() {
   ssh-add - <<< "${SSH_DEPLOY_KEY}"
 }
 
-main() {
-  setup_echo_colours
-
-  PDF_FILENAME="${BUILD_TAG:-SNAPSHOT}.pdf"
-  ZIP_FILENAME="${BUILD_TAG:-SNAPSHOT}.zip"
-  RELEASE_ARTEFACTS_DIR_NAME="release_artefacts"
-  RELEASE_ARTEFACTS_DIR="${BUILD_DIR}/${RELEASE_ARTEFACTS_DIR_NAME}"
-  RELEASE_ARTEFACTS_REL_DIR="./${RELEASE_ARTEFACTS_DIR_NAME}"
-  SITE_DIR="${BUILD_DIR}/public"
-  GITHUB_PAGES_DIR="${BUILD_DIR}/gh-pages"
-  BASE_URL_BASE="https://gchq.github.io/stroom-docs"
-
-  echo -e "BUILD_NUMBER:          [${GREEN}${BUILD_NUMBER}${NC}]"
-  echo -e "BUILD_COMMIT:          [${GREEN}${BUILD_COMMIT}${NC}]"
-  echo -e "BUILD_BRANCH:          [${GREEN}${BUILD_BRANCH}${NC}]"
-  echo -e "BUILD_TAG:             [${GREEN}${BUILD_TAG}${NC}]"
-  echo -e "BUILD_IS_RELEASE:      [${GREEN}${BUILD_IS_RELEASE}${NC}]"
-  echo -e "BUILD_IS_PULL_REQUEST: [${GREEN}${BUILD_IS_PULL_REQUEST}${NC}]"
-  echo -e "PDF_FILENAME:          [${GREEN}${PDF_FILENAME}${NC}]"
-  echo -e "ZIP_FILENAME:          [${GREEN}${ZIP_FILENAME}${NC}]"
-
-  # In order to make gitbook's pdf generation work on a headless server we need to 
-  # wrap the ebook-convert binary with our own wrapper script of the same name
-  # sudo mv ebook-convert /usr/local/bin/
-
-  #echo -e "Replacing ${GREEN}VERSION${NC} and ${GREEN}BUILD_DATE${NC}" \
-    #"tags in ${BLUE}VERSION.md${NC}"
-  #sed -i "s/@@VERSION@@/${BUILD_TAG:-SNAPSHOT}/g" VERSION.md
-  #sed -i "s/@@BUILD_DATE@@/$(date -u)/" VERSION.md
-
-  echo -e "${GREEN}Converting .puml files to .puml.svg${NC}"
-  ./container_build/runInPumlDocker.sh SVG
-
-  # TODO get this working for hugo content
-  #echo -e "${GREEN}Checking all .md files for broken links${NC}"
-  #./broken_links.sh
-
+build_version() {
+  local version="${1:-SNAPSHOT}"; shift
+  local repo_root="$1"; shift
+  
   local hugo_base_url
+  local site_dir="${repo_root}/public"
+  local pdf_filename="${version}.pdf"
+
   if [[ "${BUILD_BRANCH}" = "master" ]]; then
     hugo_base_url="${BASE_URL_BASE}/"
   else
     hugo_base_url="${BASE_URL_BASE}/${BUILD_BRANCH}"
   fi
 
-  # build the static site
+  echo -e "${GREEN}-----------------------------------------------------${NC}"
+  echo -e "${GREEN}Building ver: ${version}," \
+    "repo_root: ${repo_root}${NC}, base_url: ${hugo_base_url}"
+
+  pushd "${repo_root}"
+
+  echo -e "${GREEN}Converting .puml files to .puml.svg${NC}"
+  ./container_build/runInPumlDocker.sh SVG
+
+  # Build the Hugo site html
   # TODO, remove --buildDrafts arg once we merge to master
-  echo -e "${GREEN}Installing and building gitbook${NC}"
+  echo -e "${GREEN}Building site HTML with Hugo${NC}"
   docker-compose \
     -f ./container_build/docker_hugo/docker-compose.yaml \
     run \
     site \
     --buildDrafts \
     --baseUrl "${hugo_base_url}"
+
+  echo -e "${GREEN}Building site PDF with pupeteer${NC}"
+  ./container_build/runInPupeteerDocker.sh PDF
+  mv stroom-docs.pdf "${RELEASE_ARTEFACTS_REL_DIR}/${pdf_filename}"
+
+  echo -e "${GREEN}Copying site HTML (${BLUE}${site_dir}${GREEN}) to combined" \
+    "site (${BLUE}${COMBINED_SITE_DIR}/${version}${GREEN})${NC}"
+  cp -r "${site_dir}"/* "${COMBINED_SITE_DIR}/${version}/"
+  
+  popd
+}
+
+main() {
+  setup_echo_colours
+
+  # Array of branch names that contain a version of the site,
+  # e.g. master, 7.1, 7.0, legacy
+  RELEASE_BRANCHES=(
+    "hugo-docsy"
+  )
+  #RELEASE_BRANCHES=(
+    #"master"
+    #"7.0"
+    #"legacy"
+  #)
+
+  PDF_FILENAME="${BUILD_TAG:-SNAPSHOT}.pdf"
+  ZIP_FILENAME="${BUILD_TAG:-SNAPSHOT}.zip"
+  RELEASE_ARTEFACTS_DIR_NAME="release_artefacts"
+  RELEASE_ARTEFACTS_DIR="${BUILD_DIR}/${RELEASE_ARTEFACTS_DIR_NAME}"
+  RELEASE_ARTEFACTS_REL_DIR="./${RELEASE_ARTEFACTS_DIR_NAME}"
+  COMBINED_SITE_DIR="${BUILD_DIR}/combined_site"
+  GIT_WORK_DIR="${BUILD_DIR}/git_work"
+  SITE_DIR="${BUILD_DIR}/public"
+  GITHUB_PAGES_DIR="${BUILD_DIR}/gh-pages"
+  BASE_URL_BASE="https://gchq.github.io/stroom-docs"
+  GIT_REPO_URL="https://github.com/gchq/stroom-docs.git"
+
+  echo -e "BUILD_BRANCH:          [${GREEN}${BUILD_BRANCH}${NC}]"
+  echo -e "BUILD_COMMIT:          [${GREEN}${BUILD_COMMIT}${NC}]"
+  echo -e "BUILD_IS_PULL_REQUEST: [${GREEN}${BUILD_IS_PULL_REQUEST}${NC}]"
+  echo -e "BUILD_IS_RELEASE:      [${GREEN}${BUILD_IS_RELEASE}${NC}]"
+  echo -e "BUILD_NUMBER:          [${GREEN}${BUILD_NUMBER}${NC}]"
+  echo -e "BUILD_TAG:             [${GREEN}${BUILD_TAG}${NC}]"
+  echo -e "PDF_FILENAME:          [${GREEN}${PDF_FILENAME}${NC}]"
+  echo -e "RELEASE_BRANCHES:      [${GREEN}${RELEASE_BRANCHES[*]}${NC}]"
+  echo -e "ZIP_FILENAME:          [${GREEN}${ZIP_FILENAME}${NC}]"
+
+  mkdir -p "${RELEASE_ARTEFACTS_DIR}"
+  mkdir -p "${COMBINED_SITE_DIR}"
+
+  # Build the commit/tag/pr that triggered this script to run
+  # to ensure the site and PDF build ok.
+  build_version "${BUILD_TAG}" "${BUILD_DIR}"
+
+  #echo -e "Replacing ${GREEN}VERSION${NC} and ${GREEN}BUILD_DATE${NC}" \
+    #"tags in ${BLUE}VERSION.md${NC}"
+  #sed -i "s/@@VERSION@@/${BUILD_TAG:-SNAPSHOT}/g" VERSION.md
+  #sed -i "s/@@BUILD_DATE@@/$(date -u)/" VERSION.md
+
+  # TODO get this working for hugo content
+  #echo -e "${GREEN}Checking all .md files for broken links${NC}"
+  #./broken_links.sh
+
+  pushd "${GIT_WORK_DIR}"
+
+  local hugo_base_url
+  for branch_name in "${RELEASE_BRANCHES[@]}"; do
+
+    # We may have already built this branch if it had the commit
+    # that triggered the build, so ignore it
+    if [[ "${branch_name}" != "${BUILD_BRANCH}" ]]; then
+      echo -e "${GREEN}Cloning branch ${BLUE}${branch_name}${GREEN} of" \
+        "${BLUE}${GIT_REPO_URL}${GREEN} into ${BLUE}./${branch_name}${NC}"
+
+      git \
+        clone \
+        --depth 1 \
+        -b "${branch_name}" \
+        "${GIT_REPO_URL}" \
+        "./${branch_name}"
+
+      # Run the build for this branch in the self named dir
+      build_version "${branch_name}" "${branch_name}"
+
+    # TODO: AT # Do we want the docs for unreleased code to be on / or /master?
+      if [[ "${BUILD_BRANCH}" = "master" ]]; then
+        hugo_base_url="${BASE_URL_BASE}/"
+      else
+        hugo_base_url="${BASE_URL_BASE}/${BUILD_BRANCH}"
+      fi
+    else
+      echo -e "${GREEN}Skipping build for ${BLUE}${branch_name}${NC}"
+    fi
+  done
+
+  popd
+
+  # TODO: <13-01-22, AT> # Need to loop over all known release branches,
+  # i.e legacy, 7.0, 7.1, master and for each one:
+  #   check it out 
+  #   run the site build with the appropriate base url
+  #   run the pdf generation
+  # Then we will have a site/pdf for each version.
+  # Would only need to do this for any commits to master, maybe only
+  # tagged commits on master once we have merged everything up.
+
+  # build the static site
+  # TODO, remove --buildDrafts arg once we merge to master
+  #echo -e "${GREEN}Installing and building gitbook${NC}"
+  #docker-compose \
+    #-f ./container_build/docker_hugo/docker-compose.yaml \
+    #run \
+    #site \
+    #--buildDrafts \
+    #--baseUrl "${hugo_base_url}"
 
   # TODO do we need any kind of similr check for hugo?
 
@@ -114,7 +209,6 @@ main() {
     #exit 1
   #fi
 
-  mkdir -p "${RELEASE_ARTEFACTS_DIR}"
 
   # TODO get this working in gh actions
 
@@ -127,8 +221,8 @@ main() {
   #rm -v "${SITE_DIR}"/*.sh
   #rm -v -rf "${SITE_DIR}/.github"
 
-  echo -e "${GREEN}Making a zip of the html content${NC}"
-  pushd "${SITE_DIR}"
+  echo -e "${GREEN}Making a zip of the combined site html content${NC}"
+  pushd "${COMBINED_SITE_DIR}"
   zip -r -9 "${RELEASE_ARTEFACTS_DIR}/${ZIP_FILENAME}" ./*
   popd
 
@@ -138,7 +232,7 @@ main() {
   mkdir -p "${GITHUB_PAGES_DIR}"
 
   echo -e "${GREEN}Copying from ${SITE_DIR}/ to ${GITHUB_PAGES_DIR}/${NC}"
-  cp -r "${SITE_DIR}"/* "${GITHUB_PAGES_DIR}/"
+  cp -r "${COMBINED_SITE_DIR}"/* "${GITHUB_PAGES_DIR}/"
 
   # We release on every commit to master
   # TODO remove this temporary hugo-docsy condition
