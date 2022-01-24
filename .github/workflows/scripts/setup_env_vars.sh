11d2d6211da05e227c45c31f5e66da9c2176b346
at055612 <22818309+at055612@users.noreply.github.com>
2022-01-24 09:58:22 +0000

    Stop forced builds for scheduled jobs

--- .github/workflows/scripts/setup_env_vars.sh
+++ .github/workflows/scripts/setup_env_vars.sh
@@ -13,9 +13,11 @@ if [[ ${GITHUB_REF} =~ ^refs/heads/ ]]; then
 fi
 
 # If releases are only done nighly this allows us to force one for testing
-# So just include '[publish]' in the commit msg
+# So just include '[publish]' in the commit msg.
+# Don't force if scheduled as a scheduled job just builds the latest commit
+# which may have force on it.
 is_forced_release=false
-if [[ ${build_branch} = "master" ]] \
+if [[ "${build_branch}" = "master" && "${GITHUB_EVENT_NAME}" != "schedule" ]] \
   && git --no-pager log -1 --pretty=format:"%s" \
     | head -n1 \
     | grep -q -i "\[publish\]"; then
