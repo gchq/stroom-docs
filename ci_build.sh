55b44d685ee185ba67f89d05abfd9d1d78d67927
at055612 <22818309+at055612@users.noreply.github.com>
2022-01-25 14:57:31 +0000

    Add echo

--- ci_build.sh
+++ ci_build.sh
@@ -384,6 +384,7 @@ prepare_for_release() {
 }
 
 populate_release_brances_arr() {
+  echo -e "${GREEN}Getting list of release branches${NC}"
   # Read all the matching release branches into the arr
   readarray -t release_branches \
     < <(curl \
