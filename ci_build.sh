e0439b709b29b3fc5d896043e5b2142c2cd4bc77
at055612 <22818309+at055612@users.noreply.github.com>
2022-11-23 16:27:51 +0000

    Add more echo

--- ci_build.sh
+++ ci_build.sh
@@ -352,6 +352,9 @@ remove_unwanted_sections() {
 set_meta_robots_for_all_version_branches() {
   local combined_site_root_dir="$1"; shift
 
+  echo -e "${GREEN}Setting meta robots for all versioned brances in" \
+    "${BLUE}${combined_site_root_dir}${NC}"
+
   for branch_name in "${release_branches[@]}"; do
     local site_html_root_dir="${combined_site_root_dir}/${branch_name}"; shift
 
